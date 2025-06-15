19997f86d5844ef2249b4054934ff9f8a8bc8497
Okenamay <yamabaka@yandex.ru>
2025-06-15 23:34:02 +0300

    Инкремент 10 коммит 9.
    Вместо централизованной работы с БД, создаю pgxpool и делаю Ping() непосредственно в хендлере PingHandler.

--- internal/server/handlers/pinger.go
+++ internal/server/handlers/pinger.go
@@ -15,29 +15,21 @@ func PingHandler(conf *config.Cfg) http.HandlerFunc {
 		sugar, _ := logger.InitLogger()
 		sugar.Info("PingHandler. Start")
 
-		// Проверяем подключение к БД
-		sugar.Info("PingHandler. тест 0")
-		// err := database.DBPool.Ping(context.Background())
 		DBPool, err := pgxpool.New(context.Background(), conf.PostgreDSN)
 		if err != nil {
-			sugar.Info("PingHandler. тест 5")
 			sugar.Errorw("PingHandler. DB pool error", "error", err)
 			http.Error(w, "Database connection error", http.StatusInternalServerError)
 			return
 		}
 
 		err = DBPool.Ping(context.Background())
-		sugar.Info("PingHandler. тест 1")
 		if err != nil {
-			sugar.Info("PingHandler. тест 2")
 			sugar.Errorw("PingHandler. DB ping error", "error", err)
 			http.Error(w, "Database connection error", http.StatusInternalServerError)
 			return
 		}
 
-		sugar.Info("PingHandler. тест 3")
 		w.WriteHeader(http.StatusOK)
-		sugar.Info("PingHandler. тест 4")
 		w.Write([]byte("PONG"))
 	}
 }
