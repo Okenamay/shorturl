4dc0de43458a60a6e652b55dcc84fc426404f21e
Okenamay <yamabaka@yandex.ru>
2025-07-06 15:59:08 +0300

    Инкремент 13 после исправлений коммит 21.
    Дебаг. Пофиксил кусочек лога.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -20,7 +20,7 @@ func main() {
 	}
 	defer memselect.MemStop(conf)
 
-	logger.Zap.Infow("Starting server on port: ", conf.ServerPort)
+	logger.Zap.Infof("Starting server on port: ", conf.ServerPort)
 
 	err = router.Launch(conf)
 	if err != nil {
