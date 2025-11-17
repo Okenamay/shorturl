6adef2e824f04c521bb0ad1321c0b00db84d375f
Okenamay <yamabaka@yandex.ru>
2025-11-17 15:31:28 +0300

    Инкремент 23 коммит 2.
    1) Добавил stdout сообщения.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/Okenamay/shorturl.git/internal/audit"
@@ -11,7 +12,25 @@ import (
 	"github.com/Okenamay/shorturl.git/internal/worker"
 )
 
+// Переменные для информации о сборке (будут установлены через -ldflags)
+var buildVersion string
+var buildDate string
+var buildCommit string
+
 func main() {
+	// Вспомогательная функция для вывода "N/A", если переменная пуста
+	na := func(s string) string {
+		if s == "" {
+			return "N/A"
+		}
+		return s
+	}
+
+	// Выводим информацию о сборке в stdout
+	fmt.Printf("Build version: %s\n", na(buildVersion))
+	fmt.Printf("Build date: %s\n", na(buildDate))
+	fmt.Printf("Build commit: %s\n", na(buildCommit))
+
 	appLogger, err := logger.InitLogger()
 	if err != nil {
 		// Если логгер не стартовал, мы не можем даже это залогировать. Паникуем.
